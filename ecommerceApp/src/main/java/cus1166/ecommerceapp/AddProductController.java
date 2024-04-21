514a7613f7531c1e85275a0279022c164d039c52
NateDorsey <nathanael.dorsey20@my.stjohns.edu>
2024-04-21 19:31:12 -0400

    Fixed navigation to addproducts.fxml

--- ecommerceApp/src/main/java/cus1166/ecommerceapp/AddProductController.java
+++ ecommerceApp/src/main/java/cus1166/ecommerceapp/AddProductController.java
@@ -7,8 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -17,7 +16,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import models.Product;
 
-import java.awt.*;
+
 import java.io.*;
 import java.net.URL;
 import java.sql.Connection;
